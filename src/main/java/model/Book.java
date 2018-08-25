c1ba5dcdb36979bf5c39137bc79d6e5f5274419e
Piotr Babiuch <pbabiuch@taxusit.com.pl>
2018-08-25 17:28:46 +0200

    model książki

---
+++ src/main/java/model/Book.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Book {
+
+    private Integer id;
+    private String title;
+    private Integer year;
+    private String author;
+
+    public Book(int id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
